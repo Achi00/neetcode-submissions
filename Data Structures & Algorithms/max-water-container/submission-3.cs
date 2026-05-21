205a6937fa56c97ad3553bae761743c89cc9a19d
Archil Gorgadze <101459158+Achi00@users.noreply.github.com>
2026-05-21 21:44:13 +0400

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public int MaxArea(int[] heights) {
+        int max = 0;
+
+        int j = heights.Length - 1;
+        int i = 0;
+
+        while (i < j)
+        {
+            var min = Math.Min(heights[i], heights[j]);
+            current = (j - i) * min;
+            if (heights[i] > heights[j])
+            {
+                j--;
+            }
+            else
+            {
+                i++;
+            }
+
+            if (current > max)
+            {
+                max = current;
+            }
+        }
+
+        return max;
+    }
+}
